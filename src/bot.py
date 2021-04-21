53dd91e3d0fa86b17a061d389e9e98257247dd55
gammaseeker <joeyjiem@gmail.com>
2021-04-21 08:42:19 -0400

    refac to support variable args

--- src/bot.py
+++ src/bot.py
@@ -18,29 +18,32 @@ arg1 = class code
 arg2 = class number
 """
 @bot.command(name='drive', help='Provides google drive link of desired class')
-async def drive_cmd_handler(ctx, arg1, arg2):
-    #TODO need to regex arg2 is a 3 digit number
+async def drive_cmd_handler(ctx, *args):
     response = "If you see this msg, something went very wrong..."
-    value = arg1.upper() + ' ' + arg2
-    try:
-        record = data_accessor.get_item(key='class_name', value=value)
-        response = record['Item']['link']
-    # This exception is triggered bc 'Item' does not exist in record
-    except Exception as e:
-        response = f"Error requested course google drive not found\nContact @joeyjiem"
-    '''
-    except DynamoDB.Client.exceptions.ProvisionedThroughputExceededException:
-        response = "ProvisionedThroughputExceededException contact Joey Jiemjitpolchai"
+    if len(args) != 2:
+        response = "Error. Please format your input correctly: !drive [CLASS] [CODE]\nExample usage: !drive CSE 320"
+    else:
+        #TODO need to regex arg2 is a 3 digit number
+        value = args[0].upper() + ' ' + args[1]
+        try:
+            record = data_accessor.get_item(key='class_name', value=value)
+            response = record['Item']['link']
+        # This exception is triggered bc 'Item' does not exist in record
+        except Exception as e:
+            response = f"Error requested course google drive not found\nContact @joeyjiem"
+        '''
+        except DynamoDB.Client.exceptions.ProvisionedThroughputExceededException:
+            response = "ProvisionedThroughputExceededException contact Joey Jiemjitpolchai"
 
-    except DynamoDB.Client.exceptions.ResourceNotFoundException:
-        response = "ResourceNotFoundException contact Joey Jiemjitpolchai"
+        except DynamoDB.Client.exceptions.ResourceNotFoundException:
+            response = "ResourceNotFoundException contact Joey Jiemjitpolchai"
 
-    except DynamoDB.Client.exceptions.RequestLimitExceeded:
-        response = "RequestLimitExceeded contact Joey Jiemjitpolchai"
+        except DynamoDB.Client.exceptions.RequestLimitExceeded:
+            response = "RequestLimitExceeded contact Joey Jiemjitpolchai"
 
-    except DynamoDB.Client.exceptions.InternalServerError:
-        response = "InternalServerError contact Joey Jiemjitpolchai"
-    '''
+        except DynamoDB.Client.exceptions.InternalServerError:
+            response = "InternalServerError contact Joey Jiemjitpolchai"
+        '''
     await ctx.send(response)
 
 bot.run(TOKEN)
